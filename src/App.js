06596a0e05c8f5dd81ddc8a1d30e2f2fced97d23
ese_babyy <joshuaese075@gmail.com>
2026-05-28 04:58:16 +0100

    Revert "finished hero section"
    This reverts commit e8b08a0fcd2c2c8efbc07d50f732a3f557a74553.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import Hero from  './components/Hero';
-export default function Home() {
-  return(
-    <main>
-      <Hero />
-      <section id="planetsCards">
-        </section>
-       <section id="contactForm">
-        </section>
-    </main>
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 
- 
