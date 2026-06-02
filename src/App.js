788c94982c5f176af75d1817ea416ce29c5e3de0
izic1234 <yisoladipe@gmail.com>
2026-06-01 23:00:11 -0700

    omoyisola navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Planettable from './components/Planettable';
 import ContactForm from './components/Contactform';
@@ -6,6 +7,7 @@ import VideoSection from './components/Videosection';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
       <VideoSection />
       <Planettable />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import'./Navbar.css';
+function Navbar() {
+    return(
+        <>
+        <section className="projectlogo">
+            <figure>
+          <img src="/graphics.png" alt="capstone logo" ></img>
+          </figure>
+        </section>
+        </>
+    )
+}
+
+export default Navbar;
