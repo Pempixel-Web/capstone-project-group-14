3fb6b09467e29b4eaf47f3e729bce0a9c7d4a24f
Vincent Ezekiel <tech.pempixel@gmail.com>
2026-06-04 23:01:12 +0100

    fix App.js merge conflict

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Planettable from './components/Planettable';
 import ContactForm from './components/Contactform';
 import VideoSection from './components/VideoSection';
 import Footer from './components/Footer';
+import PlanetCard from './components/PlanetCard';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <VideoSection />
+      <PlanetCard/>
       <Planettable />
       <ContactForm />
       <Footer />
