43fd902f9a796157a463c6dc527fae1f819f85fd
peru <soyodeomotola@gmail.com>
2026-06-04 22:09:00 +0100

    updated the video section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Planettable from './components/Planettable';
 import ContactForm from './components/Contactform';
-import VideoSection from './components/Videosection';
+import VideoSection from './components/VideoSection';
 import Footer from './components/Footer';
 function App() {
   return (
